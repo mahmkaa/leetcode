2dff1f2f7ecc39b0b131848704de43a9cb0f3c4e
Pavel <cubertx1@gmail.com>
2024-06-20 18:57:23 +0700

    80. Remove Duplicates from Sorted Array II

--- 80. Remove Duplicates from Sorted Array II.playground/Contents.swift
+++ 80. Remove Duplicates from Sorted Array II.playground/Contents.swift
@@ -1,1 +1,19 @@
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    var index = 2
+    var k = 0
+    
+    while index < nums.count {
+        if nums[index] == nums[index - 2]{
+            nums.remove(at: index)
+        } else {
+            index += 1
+        }
+    }
+    
+    k = nums.count
+    return k
+}
 
+var nums = [1,1,2,2,2,3,4,4,4,5,6,6,6]
+removeDuplicates(&nums)
+print("\(nums)")
